4877581e4aa0cb3a6b786f50177a5943d644f51e
Raul Barbosa <barbosamartins94@gmail.com>
2024-03-20 21:01:23 -0300

    feat: table component created

--- src/pages/transactions/index.tsx
+++ src/pages/transactions/index.tsx
@@ -1,11 +1,30 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width={"50%"}>Desenvolvimento de site</td>
+              <td><PriceHighLight variant={"income"}>12.000,00</PriceHighLight></td>
+              <td>Venda</td>
+              <td>13/04/2024</td>
+            </tr>
+            <tr>
+              <td width={"50%"}>Ar condicionado</td>
+              <td><PriceHighLight variant={"outcome"}>3.000,00</PriceHighLight></td>
+              <td>Compra</td>
+              <td>13/04/2024</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   )
 }
